bcfa58a761b5bb8ad98ba713a53cad92d7044163
= <danielmonteiro@id.uff.br>
2016-02-09 21:51:15 -0200

    Add code to retrieve the image from provided Url
    But it still has to return it to the requester.

--- asynceventhandlercore/src/main/java/br/odb/asynceventhandlercore/requestCatImage.java
+++ asynceventhandlercore/src/main/java/br/odb/asynceventhandlercore/requestCatImage.java
@@ -1,5 +1,12 @@
 package br.odb.asynceventhandlercore;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by monty on 09/02/16.
@@ -16,5 +23,15 @@ public class requestCatImage extends AbstractAsyncEvent {
     @Override
     public void perform() {
 
+        URL url = null;
+        try {
+            url = new URL(mCatImageUrl);
+            URLConnection connection = url.openConnection();
+            Bitmap bmp = BitmapFactory.decodeStream( connection.getInputStream());
+        } catch (MalformedURLException e) {
+            reportFailure();
+        } catch (IOException e) {
+            reportFailure();
+        }
     }
 }
